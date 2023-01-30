cdc842c24eb8cd161116d9c0c26b23c6a6260e01
Luke Skywalker <skywalker.eth@protonmail.com>
2023-01-30 17:45:22 +0530

    revamp: improved the code modularity

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-require("isomorphic-fetch");
 require("process");
 
-const rwClient = require("./twitterClient.js");
-const { formatFloat, toDollar, getToday, farmUrl } = require("./utils");
+const {
+  getToday,
+  getTweetText,
+  getReplyText,
+  tweeter,
+  fetchData,
+} = require("./utils");
 
-let farms_apr, farms_safety, farms_tvl;
+let farms_apr, farms_score, farms_tvl;
 let tweet_text_tvl, reply_text_tvl;
 let tweet_text_apr, reply_text_apr;
 let tweet_text_score, reply_text_score;
@@ -12,73 +16,21 @@ let tweet_text_score, reply_text_score;
 // Final function that uploads media and sends tweet requests
 const tweet = async () => {
   console.log("tweeting...");
-  try {
-    const mediaID_tvl = await rwClient.v1.uploadMedia("assets/tvl.png");
-    const mediaID_apr = await rwClient.v1.uploadMedia("assets/yield.png");
-    const mediaID_safety = await rwClient.v1.uploadMedia("assets/safety.png");
 
-    // Top TVL Farms
-    const { data: tweet_tvl } = await rwClient.v2.tweet({
-      text: tweet_text_tvl,
-      media: { media_ids: [mediaID_tvl] },
-    });
-    await rwClient.v2.reply(reply_text_tvl, tweet_tvl.id);
-
-    // Top APR Farms
-    const { data: tweet_apr } = await rwClient.v2.tweet({
-      text: tweet_text_apr,
-      media: { media_ids: [mediaID_apr] },
-    });
-    await rwClient.v2.reply(reply_text_apr, tweet_apr.id);
-
-    // Top Safety Farms
-    const { data: tweet_score } = await rwClient.v2.tweet({
-      text: tweet_text_score,
-      media: { media_ids: [mediaID_safety] },
-    });
-    await rwClient.v2.reply(reply_text_score, tweet_score.id);
-  } catch (e) {
-    console.error(e);
-  }
+  await tweeter("tvl", tweet_text_tvl, reply_text_tvl);
+  await tweeter("yield", tweet_text_apr, reply_text_apr);
+  await tweeter("score", tweet_text_score, reply_text_score);
 };
 
 const main = async () => {
-  console.log("fetching api...");
-  const data = await (
-    await fetch(process.env.API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `
-          {
-            farms {
-              id
-              chain
-              tvl
-              protocol
-              asset {
-                symbol
-                address
-              }
-              safetyScore
-              apr {
-                base
-                reward
-              }
-            }
-          }`,
-      }),
-    })
-  ).json();
-
-  let farms = data.data.farms;
-
+  // Data of all tracked farms
+  let farms = await fetchData();
   farms_tvl = await farms.slice(0, 3);
 
   farms.sort(function (a, b) {
     return b.safetyScore - a.safetyScore;
   });
-  farms_safety = await farms.slice(0, 3);
+  farms_score = await farms.slice(0, 3);
 
   farms.sort(function (a, b) {
     return b.apr.reward + b.apr.base - a.apr.reward - a.apr.base;
@@ -89,90 +41,25 @@ const main = async () => {
   str_today = getToday();
 
   // Main tweet string formation
-  tweet_text_tvl = `GM Sailors 🌊\n\nHighest TVL Farms on list.yieldbay.io (${str_today})  ↓\n\n→ ${
-    farms_tvl[0].protocol.charAt(0).toUpperCase() +
-    farms_tvl[0].protocol.slice(1)
-  } on ${
-    farms_tvl[0].chain.charAt(0).toUpperCase() + farms_tvl[0].chain.slice(1)
-  } : ${farms_tvl[0].asset.symbol}  -  ${toDollar(farms_tvl[0].tvl)}\n→ ${
-    farms_tvl[1].protocol.charAt(0).toUpperCase() +
-    farms_tvl[1].protocol.slice(1)
-  } on ${
-    farms_tvl[1].chain.charAt(0).toUpperCase() + farms_tvl[1].chain.slice(1)
-  } : ${farms_tvl[1].asset.symbol}  -  ${toDollar(farms_tvl[1].tvl)}\n→ ${
-    farms_tvl[2].protocol.charAt(0).toUpperCase() +
-    farms_tvl[2].protocol.slice(1)
-  } on ${
-    farms_tvl[2].chain.charAt(0).toUpperCase() + farms_tvl[2].chain.slice(1)
-  } : ${farms_tvl[2].asset.symbol}  -  ${toDollar(farms_tvl[2].tvl)}`;
+  tweet_text_tvl = `GM Sailors 🌊\n\nHighest TVL Farms on list.yieldbay.io (${str_today})  ↓\n\n${getTweetText(
+    farms_tvl,
+    "tvl"
+  )}`;
 
-  tweet_text_apr = `Want to maximise your yields?\n\nFarms with highest yields listed on list.yieldbay.io (${str_today})  ↓\n\n→ ${
-    farms_apr[0].protocol.charAt(0).toUpperCase() +
-    farms_apr[0].protocol.slice(1)
-  } on ${
-    farms_apr[0].chain.charAt(0).toUpperCase() + farms_apr[0].chain.slice(1)
-  } : ${farms_apr[0].asset.symbol}  -  ${formatFloat(
-    farms_apr[0].apr.reward + farms_apr[0].apr.base
-  )}%\n→ ${
-    farms_apr[1].protocol.charAt(0).toUpperCase() +
-    farms_apr[1].protocol.slice(1)
-  } on ${
-    farms_apr[1].chain.charAt(0).toUpperCase() + farms_apr[1].chain.slice(1)
-  } : ${farms_apr[1].asset.symbol}  -  ${formatFloat(
-    farms_apr[1].apr.reward + farms_apr[1].apr.base
-  )}%\n→ ${
-    farms_apr[2].protocol.charAt(0).toUpperCase() +
-    farms_apr[2].protocol.slice(1)
-  } on ${
-    farms_apr[2].chain.charAt(0).toUpperCase() + farms_apr[2].chain.slice(1)
-  } : ${farms_apr[2].asset.symbol}  -  ${formatFloat(
-    farms_apr[2].apr.reward + farms_apr[2].apr.base
-  )}%`;
+  tweet_text_apr = `Want to maximise your yields?\n\nFarms with highest yields listed on list.yieldbay.io (${str_today})  ↓\n\n${getTweetText(
+    farms_apr,
+    "apr"
+  )}`;
 
-  tweet_text_score = `Here are the highest ranked yield farming opportunities on list.yieldbay.io (${str_today})  ↓\n\n→ ${
-    farms_safety[0].protocol.charAt(0).toUpperCase() +
-    farms_safety[0].protocol.slice(1)
-  } on ${
-    farms_safety[0].chain.charAt(0).toUpperCase() +
-    farms_safety[0].chain.slice(1)
-  } : ${farms_safety[0].asset.symbol}  -  ${formatFloat(
-    farms_safety[0].safetyScore * 10
-  )}/10\n→ ${
-    farms_safety[1].protocol.charAt(0).toUpperCase() +
-    farms_safety[1].protocol.slice(1)
-  } on ${
-    farms_safety[1].chain.charAt(0).toUpperCase() +
-    farms_safety[1].chain.slice(1)
-  } : ${farms_safety[1].asset.symbol}  -  ${formatFloat(
-    farms_safety[1].safetyScore * 10
-  )}/10\n→ ${
-    farms_safety[2].protocol.charAt(0).toUpperCase() +
-    farms_safety[2].protocol.slice(1)
-  } on ${
-    farms_safety[2].chain.charAt(0).toUpperCase() +
-    farms_safety[2].chain.slice(1)
-  } : ${farms_safety[2].asset.symbol}  -  ${formatFloat(
-    farms_safety[2].safetyScore * 10
-  )}/10`;
+  tweet_text_score = `Here are the highest ranked yield farming opportunities on list.yieldbay.io (${str_today})  ↓\n\n${getTweetText(
+    farms_score,
+    "score"
+  )}`;
 
   // Reply tweet with farms
-  reply_text_tvl = `Here you can find more about these farms ↓\n\n→ ${
-    farms_tvl[0].asset.symbol
-  }  - ${farmUrl(farms_tvl[0])}\n→ ${farms_tvl[1].asset.symbol}  -  ${farmUrl(
-    farms_tvl[1]
-  )}\n→ ${farms_tvl[2].asset.symbol}  -  ${farmUrl(farms_tvl[2])}`;
-
-  reply_text_apr = `Here you can find more about these farms ↓\n\n→ ${
-    farms_apr[0].asset.symbol
-  }  - ${farmUrl(farms_apr[0])}\n→ ${farms_apr[1].asset.symbol}  -  ${farmUrl(
-    farms_apr[1]
-  )}\n→ ${farms_apr[2].asset.symbol}  -  ${farmUrl(farms_apr[2])}`;
-  
-  reply_text_score = `Here you can find more about these farms ↓\n\n→ ${
-    farms_safety[0].asset.symbol
-  }  - ${farmUrl(farms_safety[0])}\n→ ${farms_safety[1].asset.symbol}  -  ${farmUrl(
-    farms_safety[1]
-  )}\n→ ${farms_safety[2].asset.symbol}  -  ${farmUrl(farms_safety[2])}`;
+  reply_text_tvl = getReplyText(farms_tvl);
+  reply_text_apr = getReplyText(farms_apr);
+  reply_text_score = getReplyText(farms_score);
 
   //Call tweet
   await tweet();
--- utils.js
+++ utils.js
@@ -1,3 +1,8 @@
+require("isomorphic-fetch");
+require("process");
+
+const rwClient = require("./twitterClient.js");
+
 function formatFloat(value) {
   return parseFloat(value).toFixed(2);
 }
@@ -41,9 +46,87 @@ function farmUrl(farm) {
   );
 }
 
+function getTweetText(farms, type) {
+  let text = "";
+  for (let i = 0; i < 3; i++) {
+    text += `→ ${
+      farms[i].protocol.charAt(0).toUpperCase() + farms[i].protocol.slice(1)
+    } on ${
+      farms[i].chain.charAt(0).toUpperCase() + farms[i].chain.slice(1)
+    } : ${farms[i].asset.symbol}  -  `;
+    if (type == "tvl") {
+      text += `${toDollar(farms[i].tvl)}\n`;
+    } else if (type == "apr") {
+      text += `${formatFloat(farms[i].apr.reward + farms[i].apr.base)}%\n`;
+    } else {
+      text += `${formatFloat(farms[i].safetyScore * 10)}/10\n`;
+    }
+  }
+  return text;
+}
+
+function getReplyText(farms) {
+  let text = "Here you can find more about these farms ↓\n\n";
+  for (let i = 0; i < 3; i++) {
+    text += `→ ${farms[i].asset.symbol}  - ${farmUrl(farms[i])}\n`;
+  }
+  return text;
+}
+
+async function tweeter(media, tweet_text, reply_text) {
+  try {
+    const mediaID = await rwClient.v1.uploadMedia(`assets/${media}.png`);
+    const { data: tweet } = await rwClient.v2.tweet({
+      text: tweet_text,
+      media: { media_ids: [mediaID] },
+    });
+    await rwClient.v2.reply(reply_text, tweet.id);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function fetchData() {
+  console.log("fetching api...");
+  console.log(process.env.API_URL);
+  const data = await (
+    await fetch(process.env.API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+          {
+            farms {
+              id
+              chain
+              tvl
+              protocol
+              asset {
+                symbol
+                address
+              }
+              safetyScore
+              apr {
+                base
+                reward
+              }
+            }
+          }`,
+      }),
+    })
+  ).json();
+
+  let farms = data.data.farms;
+  return farms;
+}
+
 module.exports = {
   formatFloat,
   toDollar,
   getToday,
   farmUrl,
+  getTweetText,
+  getReplyText,
+  tweeter,
+  fetchData,
 };
